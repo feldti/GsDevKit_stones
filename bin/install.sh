7fa94b21f5f73f5503fbb3deac70a9fc3a2610f6
mf <marten@feldtmann.online>
2024-11-26 15:29:39 +0100

    Zugriffe auf git vereinfacht

--- bin/install.sh
+++ bin/install.sh
@@ -14,12 +14,12 @@ fi
 gsDevKitStones="`dirname $0`/.."
 cd $gsDevKitStones/..
 
-if [ "$CI" = "true" ]; then
+#if [ "$CI" = "true" ]; then
 	# set CI to true to use https to clone superDoit using https instead of ssh"
 	gitUrl="https://github.com/feldti/superDoit.git"
-else
-	gitUrl="git@github.com:feldti/superDoit.git"
-fi
+#else
+#	gitUrl="git@github.com:feldti/superDoit.git"
+#fi
 git clone $gitUrl -b v4.2
 cd superDoit/bin
 ./install.sh $1
