7d215ec1a5ea514ce4283f09161397aba3d83cbf
mf <marten@feldtmann.online>
2025-04-19 16:07:14 +0200

    Korrekturen beim Laden des Topaz Codes

--- bin/pas_load_topaz_source.sh
+++ bin/pas_load_topaz_source.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 # Dieses Skript loads a topaz script produced by PUM describing a model
 #
+export BASEPATH="http://192.168.178.170/app-sources"
+
 usage() {
   cat <<HELP
 
 USAGE: $(basename $0) <stone-name> <registry-name> <softwarename> <branch> <version>
 
-Imports new model source from topaz file
+Imports new model source from topaz file located at host $BASEPATH
 $1 = Name of target stone database
-$2 = Name of registry
-$3 = Name of the software application
+$2 = Name of registry (e.g. work)
+$3 = Name of the software application (e.g. sm)
 $4 = Name of Branch (e.g. v00)
 $5 = Specific Version (e.g. 0_001)
 
 
 EAXMPLES
-  $(basename $0) v00 0_001 gc develop|production
+  $(basename $0) testv100 work sm v00 0_001
 
 HELP
 }
@@ -23,7 +25,7 @@ if [ $# -ne 5 ]; then
   usage; exit 1
 fi
 
-export BASEPATH="http://192.168.178.170/app-sources"
+
 rm out.txt
 echo "Retrieving data from: "$BASEPATH/$3/$4/sources/$3-modelsource-$5.sh
 curl $BASEPATH/$3/$4/sources/$3-modelsource-$5.sh > $3-$2.sh
