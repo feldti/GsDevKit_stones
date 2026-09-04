8750589c5282a0c20e57bedf01b6bf523c672722
mf <marten@feldtmann.online>
2026-09-04 13:56:31 +0200

    Neue Skripte

---
+++ bin/pas_start_slave.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+#
+# This script starts the logsender for the specific host
+#
+#
+if [ ! -f "./credentials.sh" ]; then
+    echo "credentials.sh file not available"
+    exit 4
+fi
+source ./credentials.sh
+
+
+stoneName=$PAS_STONE_NAME
+registryName=$PAS_STONE_REGISTRY
+stonesDataHome=$STONES_DATA_HOME
+
+# Extract the value of 'stone_dir' from the .ston file
+stone_dir=$(pas_datadir.sh $stoneName $registryName $stonesDataHome)
+# Check the return code of the script
+if [[ $? -eq 0 ]]; then
+    echo ""
+else
+    echo "The script failed with return code $?."
+fi
+# Check if stone_dir was found
+if [[ -z "$stone_dir" ]]; then
+    echo "Error: 'stone_dir' not found in $ston_file_path"
+    exit 1
+fi
+source $stone_dir/customenv
+if [ -s $GEMSTONE/seaside/etc/gemstone.secret ]; then
+    . $GEMSTONE/seaside/etc/gemstone.secret
+else
+    echo 'Missing password file $GEMSTONE/seaside/etc/gemstone.secret'
+    exit 1
+fi
+
+nowTS=`date +%Y-%m-%d-%H-%M`
+if [ "$HOTSTANDBYBYENABLED" = "true" ]; then
+  nohup $GEMSTONE/bin/startlogreceiver -P $HOTSTANDBYMASTERPORT -A $HOTSTANDBYMASTERADR  -T $HOTSTANDBYSOURCE -s $PAS_STONE_NAME -l $GEMSTONE_LOGDIR &
+  echo "LOGRECEIVER started"
+fi
+#
+#
+#
+cat << EOF | $GEMSTONE/bin/topaz -lq -T 1000000 -u pum_continous_restore
+set user DataCurator pass $GEMSTONE_CURATOR_PASS gems $stoneName
+iferror where
+login
+run
+SystemRepository continuousRestoreFromArchiveLogs:{'$HOTSTANDBYSOURCE'}
+%
+EOF
